f7da6bea3914dfc4e33043d4cef1ed603ca7ae57
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-03-03 21:25:22 +0200

    Update card.h

--- Payment Application/CARD/card.h
+++ Payment Application/CARD/card.h
@@ -35,8 +35,9 @@ typedef enum EN_cardState_t
 
 /* ---------- SECTION FUNCTIONS PROTOTYPES ---------- */
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData);
-EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
 EN_cardError_t getCardPAN(ST_cardData_t* cardData);
+EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
+
 uint8_t* LuhnNumberGenerator();
 int LuhnNumberChecker(uint8_t* cardPan);
 
