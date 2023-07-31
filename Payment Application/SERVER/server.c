fde5331aaf519ec479db2e9148a6b3061eec6766
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-31 22:11:25 +0300

    Update server.c

--- Payment Application/SERVER/server.c
+++ Payment Application/SERVER/server.c
@@ -9,7 +9,6 @@ ST_accountsDB_t DataBaseAcounts[255];
 Instead of a global array create a text file "Transactions DB.txt" where you will save all transactions and read if you need
  
 */
-
  
 void getDataBaseAcounts(void)
 {
